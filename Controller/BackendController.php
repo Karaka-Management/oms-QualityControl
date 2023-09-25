1d3fd8c2d79daf36909fa8a3f55d770facb1fd12
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 15:14:06 +0000

    fix tests

---
+++ Controller/BackendController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\QualityControl
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\QualityControl\Controller;
+
+/**
+ * QualityControl controller class.
+ *
+ * @package Modules\QualityControl
+ * @license OMS License 2.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+final class BackendController extends Controller
+{
+}
--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -21,7 +21,7 @@ final class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const NAME = 'QualityControl';
 
-    protected const URI_LOAD = '';
+    protected const URI_LOAD = 'http://127.0.0.1/en/backend/qualitymanagement/qualitycontrol';
 
     use \tests\Modules\ModuleTestTrait;
 }
